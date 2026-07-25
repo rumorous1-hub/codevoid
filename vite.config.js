b86096b1b93289c4452e1a5115fc3bb0ce0fa6e3
heckerinho <achilero47@gmail.com>
2026-07-26 01:25:55 +0200

    Configure custom domain and update base path

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/codevoid/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
