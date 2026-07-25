b9af7c4865922b83aad2ddc90de053fb2fbff031
heckerinho <achilero47@gmail.com>
2026-07-26 00:31:06 +0200

    Switch to HashRouter to fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// Changed BrowserRouter to HashRouter here:
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { AuthProvider } from '@/lib/AuthContext';
 import Home from "@/pages/Home";
 import PageNotFound from './lib/PageNotFound';
@@ -7,8 +8,8 @@ import PageNotFound from './lib/PageNotFound';
 export default function App() {
   return (
     <AuthProvider>
-      {/* Add the basename right here so the router knows where it lives */}
-      <Router basename="/codevoid">
+      {/* Removed basename, HashRouter handles it automatically */}
+      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="*" element={<PageNotFound />} />
